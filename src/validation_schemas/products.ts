73b4b3eca7fa1c18d234a89879dafa4fd5278107
Angel Garcia <adjcg15@gmail.com>
2024-12-01 17:36:43 -0600

    fear(validation schemas): defines validation schema for get products in store endpoint

--- src/validation_schemas/products.ts
+++ src/validation_schemas/products.ts
@@ -1,26 +1,48 @@
 import { Schema } from "express-validator";
 
-const getProductsValidationSchema: Schema = {
+const getProductsInStoreValidationSchema: Schema = {
+    idStore: {
+        in: ["params"],
+        isInt: {
+            options: { min: 1 },
+            errorMessage: "Parameter idStore must be a positive integer",
+        },
+        toInt: true
+    },
     offset: {
         in: ["query"],
-        trim: true,
         optional: { options: { nullable: true } },
-        isNumeric: {
-            errorMessage: "Offset has an invalid format",
+        isInt: {
+            options: { min: 1 },
+            errorMessage: "Query value offset must be a positive integer",
         },
         toInt: true
     },
     limit: {
         in: ["query"],
-        trim: true,
         optional: { options: { nullable: true } },
-        isNumeric: {
-            errorMessage: "Limit has an invalid format",
+        isInt: {
+            options: { min: 1 },
+            errorMessage: "Query value limit must be a positive integer",
         },
         toInt: true
     },
+    query: {
+        in: ["query"],
+        trim: true,
+        optional: { options: { nullable: true } }
+    },
+    categoryFilter: {
+        in: ["query"],
+        optional: { options: { nullable: true } },
+        isInt: {
+            options: { min: 1 },
+            errorMessage: "Query value categoryFilter must be a positive integer",
+        },
+        toInt: true
+    }
 }
 
 export {
-    getProductsValidationSchema
+    getProductsInStoreValidationSchema
 };
