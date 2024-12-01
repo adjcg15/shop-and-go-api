3e8901ec0fee40a59b0c84bcdcdbb26c1edca84f
Angel Garcia <adjcg15@gmail.com>
2024-12-01 11:39:44 -0600

    config(database): creates pending database types definitions

--- src/types/interfaces/db.ts
+++ src/types/interfaces/db.ts
@@ -5,6 +5,14 @@ import Store from "../../models/Store";
 import Inventory from "../../models/Inventory";
 import Employee from "../../models/Employee";
 import EmployeePosition from "../../models/EmployeePosition";
+import PaymentMethod from "../../models/PaymentMethod";
+import Issuer from "../../models/Issuer";
+import Client from "../../models/Client";
+import Address from "../../models/Address";
+import Order from "../../models/Order";
+import OrderStatus from "../../models/OrderStatus";
+import Incident from "../../models/Incident";
+import OrderProduct from "../../models/OrderProduct";
 
 interface IDBModel {
     associate: (db: IDB) => void;
@@ -21,6 +29,14 @@ interface IDB {
     Inventory: typeof Inventory & IDBModel;
     Employee: typeof Employee & IDBModel;
     EmployeePosition: typeof EmployeePosition & IDBModel;
+    PaymentMethod: typeof PaymentMethod & IDBModel;
+    Issuer: typeof Issuer & IDBModel;
+    Client: typeof Client & IDBModel;
+    Address: typeof Address & IDBModel;
+    Order: typeof Order & IDBModel;
+    OrderStatus: typeof OrderStatus & IDBModel;
+    Incident: typeof Incident & IDBModel;
+    OrderProduct: typeof OrderProduct & IDBModel;
     sequelize: Sequelize;
 };
 
