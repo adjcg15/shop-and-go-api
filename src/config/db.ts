2f62ab14ba8c7998c51faa5c88966d38dceae2c4
Angel Garcia <adjcg15@gmail.com>
2024-12-01 22:46:31 -0600

    fix(db): changes database configuration to use enviroment variables in test and production enviroment

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,28 +4,28 @@ dotenv.config();
 
 const dbConfig: IDBEnviroment = {
     development: {
-        username: process.env.DB_USER || "",
-        password: process.env.DB_PASSWORD || "",
-        database: process.env.DB_NAME || "",
-        host: process.env.DB_HOST || "",
-        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER_DEV || "",
+        password: process.env.DB_PASSWORD_DEV || "",
+        database: process.env.DB_NAME_DEV || "",
+        host: process.env.DB_HOST_DEV || "",
+        port: Number(process.env.DB_PORT_DEV),
         dialect: "mssql",
         logging: false
     },
     test: {
-        username: "root",
-        password: "password",
-        database: "database_test",
-        host: "127.0.0.1",
-        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER_TEST || "",
+        password: process.env.DB_PASSWORD_TEST || "",
+        database: process.env.DB_NAME_TEST || "",
+        host: process.env.DB_HOST_TEST || "",
+        port: Number(process.env.DB_PORT_TEST),
         dialect: "mssql",
         logging: false
     },
     production: {
-        username: "root",
-        password: "password",
-        database: "database_production",
-        host: "127.0.0.1",
+        username: process.env.DB_USER || "",
+        password: process.env.DB_PASSWORD || "",
+        database: process.env.DB_NAME || "",
+        host: process.env.DB_HOST || "",
         port: Number(process.env.DB_PORT),
         dialect: "mssql",
         logging: false
