332bd4c70853fef620cdcc68d5cef947f593d8f4
rodrigo19112003 <131178299+rodrigo19112003@users.noreply.github.com>
2024-12-04 18:00:39 -0600

    feat(business-logic-exception): implements error code to business logic exception

--- src/exceptions/business/BusinessLogicException.ts
+++ src/exceptions/business/BusinessLogicException.ts
@@ -1,8 +1,11 @@
 import TrustedException from "../TrustedException";
 
 class BusinessLogicException extends TrustedException {
-    constructor(message: string) {
+    public errorCode?: string;
+
+    constructor(message: string, errorCode?: string) {
         super(message, true);
+        this.errorCode = errorCode;
 
         this.name = "BusinessLogicException";
         Object.setPrototypeOf(this, BusinessLogicException.prototype);
