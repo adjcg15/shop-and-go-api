b63bc9dd23738c9a0cf19e081731a86507067a2e
Rodrigo Aguilar López <zS21013854@estudiantes.uv.mx>
2024-12-09 14:40:04 -0600

    feat(get-product-inventories-test): implements test to get product inventories endpoint

--- src/routes/products_router.ts
+++ src/routes/products_router.ts
@@ -14,7 +14,7 @@ router.get(
 );
 
 router.get(
-    "/:idProduct/inventory",
+    "/:idProduct/inventories",
     checkSchema(getProductInventoriesValidationSchema),
     getProductInventoriesByIdProductController
 );
--- src/test_data/e2e/products_test_data.ts
+++ src/test_data/e2e/products_test_data.ts
@@ -39,6 +39,51 @@ async function insertE2EGetAllProductsTestData() {
     ]);
 }
 
+async function insertE2EGetProductInventoriesTestData() {
+    const dairyCategory = await db.ProductCategory.create({ name: "Lacteos", isActive: true });
+    const cheese = await dairyCategory.createProduct({ 
+        barCode: "0987654321", 
+        name: "Queso Panela Fud 400 gr",
+        description: "Es fresco, blanco, suave y tiende a ser duradero. Su textura podría describirse mejor como una ricota firme. No tiende a derretirse por lo que puedes asarlo o colocarlo en la parrilla para comerlo directamente.",
+        salePrice: 43.33,
+        maximumAmount: 20
+    });
+    const storeXalapa = await db.Store.create({ 
+        name: "El zorro Xalapa centro",
+        address: "Dr. Rafael Lucio 28, Zona Centro, Centro, 91000 Xalapa-Enríquez, Ver.",
+        openingTime: "07:00:00.00",
+        closingTime: "22:00:00.00",
+        latitude: 19.528771,
+        longitude: -96.922336
+    });
+    const storeMartinez = await db.Store.create({ 
+        name: "El zorro Martínez centro",
+        address: "Dr. Rafael Lucio 28, Zona Centro, Centro, 91000 Martínez de la Torre, Ver.",
+        openingTime: "07:00:00.00",
+        closingTime: "22:00:00.00",
+        latitude: 19.528781,
+        longitude: -96.922346
+    });
+    const storeCarranza = await db.Store.create({ 
+        name: "El zorro carranza centro",
+        address: "Dr. Rafael Lucio 28, Zona Centro, Centro, 91000 Carranza, Ver.",
+        openingTime: "07:00:00.00",
+        closingTime: "22:00:00.00",
+        latitude: 19.528761,
+        longitude: -96.922356
+    });
+    await db.Inventory.bulkCreate([
+        { idProduct: cheese.id, stock: 24, idStore: storeXalapa.id, expirationDate: "2024-12-24" },
+        { idProduct: cheese.id, stock: 60, idStore: storeMartinez.id, expirationDate: "2024-12-12" },
+        { idProduct: cheese.id, stock: 31, idStore: storeCarranza.id, expirationDate: "2026-08-05" },
+    ]);
+
+    return {
+        idProduct: cheese.id
+    }
+}
+
 export {
-    insertE2EGetAllProductsTestData
+    insertE2EGetAllProductsTestData,
+    insertE2EGetProductInventoriesTestData
 }
