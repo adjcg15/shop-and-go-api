91565e6394d5c80ae1a80afac6d439dc160e89ae
Rodrigo Aguilar López <zS21013854@estudiantes.uv.mx>
2024-12-13 13:33:04 -0600

    feat(request_bodies): implements request body to create order

--- src/routes/orders_router.ts
+++ src/routes/orders_router.ts
@@ -4,6 +4,7 @@ import { allowRoles } from "../middlewares/access_control";
 import UserRoles from "../types/enums/user_roles";
 import { checkSchema } from "express-validator";
 import { createOrderValidationsSchema } from "../validation_schemas/orders";
+import validateRequestSchemaMiddleware from "../middlewares/schema_validator";
 
 const router = Router();
 
@@ -12,6 +13,8 @@ router.post(
     checkTokenValidity,
     allowRoles([UserRoles.CLIENT]),
     checkSchema(createOrderValidationsSchema),
+    validateRequestSchemaMiddleware,
+    
 );
 
 export default router;
