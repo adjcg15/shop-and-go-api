2bca23b71c0247d7bc706b808bd40f29e89c4e63
Rodrigo Aguilar López <zS21013854@estudiantes.uv.mx>
2024-12-20 21:24:01 -0600

    feat(get-user-profile-tests): implements test to get user profile endpoint

--- src/__tests__/api/sessions/getUserProfile.test.ts
+++ src/__tests__/api/sessions/getUserProfile.test.ts
@@ -3,63 +3,54 @@ import createApp from "../../../lib/app";
 import { Express } from "express";
 import { HttpStatusCodes } from "../../../types/enums/http";
 import db from "../../../models";
-import { ErrorMessages } from "../../../types/enums/error_messages";
 import { Sequelize } from "sequelize";
-import { insertE2ELoginTestData } from "../../../test_data/e2e/sessions_test_data";
+import { insertE2EgetUserProfileTestData } from "../../../test_data/e2e/sessions_test_data";
 
-describe("/api/sessions", () => {
+describe("/api/sessions/profile", () => {
     let app: Express;
+    let tokenClient: string = "";
+    let tokenEmployee: string = "";
 
     beforeAll(async () => {
         app = createApp();
 
         await db.sequelize.sync({ force: true });
 
-        await insertE2ELoginTestData();
+        await insertE2EgetUserProfileTestData();
+
+        const clientSessionBody = {phoneNumber: "1234567890", password: "password12345"};
+        const clientResponse = await request(app).post(`/api/sessions`).send(clientSessionBody);
+        const client = clientResponse.body;
+        tokenClient = client.token;
+
+        const employeeSessionBody = {username: "jamon12345", password: "password12345"};
+        const employeeResponse = await request(app).post(`/api/sessions`).send(employeeSessionBody);
+        const employee = employeeResponse.body;
+        tokenEmployee = employee.token;
     });
 
-    it("Should return an token and client data registered in database", async () => {
-        const body = {phoneNumber: "1234567890", password: "password12345"};
-        const response = await request(app).post(`/api/sessions`).send(body);
+    it("Should return a client data registered in database", async () => {
+        const response = await request(app).get(`/api/sessions/profile`).set("Authorization", `Bearer ${tokenClient}`);
         const client = response.body;
         
-        expect(response.status).toBe(HttpStatusCodes.CREATED);
-        expect(client.token).toEqual(expect.any(String));
+        expect(response.status).toBe(HttpStatusCodes.OK);
         expect(client.id).toEqual(expect.any(Number));
         expect(client.fullName).toEqual(expect.any(String));
         expect(client.birthdate).toEqual(expect.any(String));
         expect(client.phoneNumber).toEqual(expect.any(String));
     });
 
-    it("Should return an token and employee data registered in database", async () => {
-        const body = {username: "jamon12345", password: "password12345"};
-        const response = await request(app).post(`/api/sessions`).send(body);
+    it("Should return an employee data registered in database", async () => {
+        const response = await request(app).get(`/api/sessions/profile`).set("Authorization", `Bearer ${tokenEmployee}`);
         const employee = response.body;
         
-        expect(response.status).toBe(HttpStatusCodes.CREATED);
-        expect(employee.token).toEqual(expect.any(String));
+        expect(response.status).toBe(HttpStatusCodes.OK);
         expect(employee.id).toEqual(expect.any(Number));
         expect(employee.fullName).toEqual(expect.any(String));
         expect(employee.user).toEqual(expect.any(String));
         expect(employee.registrationDate).toEqual(expect.any(String));
         expect(employee.isAvailableForWork).toEqual(expect.any(Boolean));
         expect(employee.isActive).toEqual(expect.any(Boolean));
-    });
-
-    it("should display an error message indicating that the credentials are invalid", async () => {
-        const body = {phoneNumber: "1234567891", password: "password12345"};
-        const response = await request(app).post(`/api/sessions`).send(body);
-        
-        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
-        expect(response.body.details).toBe(ErrorMessages.INVALID_CREDENTIALS);
-    });
-
-    it("should display an error message indicating that the credentials are invalid", async () => {
-        const body = {username: "jamin3284f2d41", password: "password12345"};
-        const response = await request(app).post(`/api/sessions`).send(body);
-        
-        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
-        expect(response.body.details).toBe(ErrorMessages.INVALID_CREDENTIALS);
     });
 
     it("should display an error message indicating that the database server connection failed", async () => {
@@ -71,7 +62,7 @@ describe("/api/sessions", () => {
         });
         
         const body = {phoneNumber: "1234567890", password: "password12345"};
-        const response = await request(app).post(`/api/sessions`).send(body);
+        const response = await request(app).get(`/api/sessions/profile`).set("Authorization", `Bearer ${tokenClient}`);
         
         expect(response.status).toBe(HttpStatusCodes.INTERNAL_SERVER_ERROR);
     });
--- src/test_data/e2e/sessions_test_data.ts
+++ src/test_data/e2e/sessions_test_data.ts
@@ -18,6 +18,25 @@ async function insertE2ELoginTestData(){
     });
 }
 
+async function insertE2EgetUserProfileTestData(){
+    await db.Client.create({
+        passwordHash: "$2b$10$MqRHa10Cw5PzHIu2ihG5suOljpAlcw6i1FQKcyj2pRS2pTGa60ycC",
+        birthdate: "1990-05-15",
+        fullName: "John Doe",
+        phoneNumber: "1234567890"
+    });
+    const position = await db.EmployeePosition.create({title: "Repartidor"});
+    await position.createEmployee({
+        fullName: "Rodrigo Aguilar López",
+        user: "jamon12345",
+        passwordHash: "$2b$10$MqRHa10Cw5PzHIu2ihG5suOljpAlcw6i1FQKcyj2pRS2pTGa60ycC",
+        registrationDate: "2011-07-16",
+        isAvailableForWork: true,
+        isActive: true
+    });
+}
+
 export {
-    insertE2ELoginTestData
+    insertE2ELoginTestData,
+    insertE2EgetUserProfileTestData
 }
