a3793b2d76fe07d464f26df729a2e2c475aceef7
Angel Garcia <adjcg15@gmail.com>
2024-12-28 00:35:13 -0600

    feat(product categories list endpoint): implements conditional recovery of inactive categories when an admin is requesting the list

--- src/controllers/product_categories_controller.ts
+++ src/controllers/product_categories_controller.ts
@@ -3,10 +3,14 @@ import { NextFunction, Request, Response } from "express";
 import { HttpStatusCodes } from "../types/enums/http";
 import { getProductCategories } from "../services/product_categories_service";
 import ProductCategory from "../models/ProductCategory";
+import UserRoles from "../types/enums/user_roles";
 
 async function getProductCategoriesController(req: Request, res: Response<InferAttributes<ProductCategory>[]>, next: NextFunction) {
     try {
-        const productCategories = await getProductCategories();
+        const user = req.user;
+        const includeInactiveCategories = user?.userRole === UserRoles.ADMINISTRATOR;
+
+        const productCategories = await getProductCategories(includeInactiveCategories);
         res.status(HttpStatusCodes.OK).json(productCategories);
     } catch (error) {
         next(error);
