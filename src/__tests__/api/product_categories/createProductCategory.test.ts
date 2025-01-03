f7a37267300f4fef79b56e8c021f26b681714861
Angel Garcia <adjcg15@gmail.com>
2025-01-03 12:02:14 -0600

    test(create category endpoint): implements test to verify request body validation

--- src/__tests__/api/product_categories/createProductCategory.test.ts
+++ src/__tests__/api/product_categories/createProductCategory.test.ts
@@ -5,14 +5,20 @@ import db from "../../../models";
 import { HttpStatusCodes } from "../../../types/enums/http";
 import { signToken } from "../../../lib/token_store";
 import UserRoles from "../../../types/enums/user_roles";
+import { insertE2ECreateProductCategoryTestData } from "../../../test_data/e2e/product_categories_test_data";
+import { ErrorMessages } from "../../../types/enums/error_messages";
 
 describe("POST /api/product-categories", () => {
     let app: Express;
+    let registeredCategoryName = "";
 
     beforeAll(async () => {
         app = createApp();
 
         await db.sequelize.sync({ force: true });
+
+        const categoryRegistered = await insertE2ECreateProductCategoryTestData();
+        registeredCategoryName = categoryRegistered.categoryName;
     });
 
     it("Should avoid category update under a GUEST request", async () => {
@@ -46,5 +52,49 @@ describe("POST /api/product-categories", () => {
             .set("Authorization", `Bearer ${token}`);
 
         expect(response.status).toBe(HttpStatusCodes.FORBIDDEN);
+    });
+
+    it("Should validate empty name body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .post(`/api/product-categories`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({ name: "        ", isActive: true });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate name body value format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .post(`/api/product-categories`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({ name: "nombre_malo", isActive: true });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate isActive body value format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .post(`/api/product-categories`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({ name: "Nueva categoría", isActive: "invalido" });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate category name already in use", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .post(`/api/product-categories`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({ name: registeredCategoryName, isActive: true });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(response.body.details).toBe(ErrorMessages.DUPLICATED_PRODUCT_CATEGORY);
     });
 });
