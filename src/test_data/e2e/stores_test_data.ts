7a5a87abb5be2afda2f5f4f687724d900032abfd
Angel Garcia <adjcg15@gmail.com>
2025-01-02 23:22:47 -0600

    feat(test data): modifies insertE2EGetProductCategoeriesTestData to insert two inactive categories

--- src/test_data/e2e/stores_test_data.ts
+++ src/test_data/e2e/stores_test_data.ts
@@ -150,9 +150,11 @@ async function insertE2EGetProductsWithStockInStoreTestData() {
 }
 
 async function insertE2EGetProductCategoeriesTestData() {
-    await db.ProductCategory.create({ name: "Lacteos", isActive: true });
+    await db.ProductCategory.create({ name: "Lacteos", isActive: false });
     await db.ProductCategory.create({ name: "Enlatados", isActive: true });
-    await db.ProductCategory.create({ name: "Juguetes", isActive: true });
+    await db.ProductCategory.create({ name: "Juguetes", isActive: false });
+    await db.ProductCategory.create({ name: "Higiene personal", isActive: true });
+    await db.ProductCategory.create({ name: "Productos congelados", isActive: true });
 }
 
 async function insertE2EGetIssuingBanksTestData() {
