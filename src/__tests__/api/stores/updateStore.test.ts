58a1889c1d938177d5ccb4508969de1309337191
Angel Garcia <adjcg15@gmail.com>
2025-01-05 11:10:37 -0600

    test(updateStore): implements test to validate body and parameters information formats

--- src/__tests__/api/stores/updateStore.test.ts
+++ src/__tests__/api/stores/updateStore.test.ts
@@ -47,4 +47,279 @@ describe("PUT /api/stores/:idStore", () => {
 
         expect(response.status).toBe(HttpStatusCodes.FORBIDDEN);
     });
+
+    it("Should validate idStore parameter format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/juan`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "12:00:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate name body value format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "wrong_format",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "12:00:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate not empty name body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "12:00:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate not empty address body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "",
+                openingTime: "08:00:00",
+                closingTime: "12:00:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate openingTime body value format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00",
+                closingTime: "12:00:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate openingTime body value outside time boundaries", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "28:00:00",
+                closingTime: "12:00:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate not empty openingTime body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "",
+                closingTime: "22:00:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate closingTime body value format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "12:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate closingTime body value outside time boundaries", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "13:78:00",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate not empty closingTime body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "",
+                latitude: 19.54823,
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate latitude body value format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "22:00:00",
+                latitude: "juan",
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate not empty latitude body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "22:00:00",
+                latitude: "",
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate latitude body value outside boundaries", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "22:00:00",
+                latitude: 91,
+                longitude: -96.93329
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate longitude body value format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "22:00:00",
+                latitude: 19.54823,
+                longitude: "juan"
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate not empty longitude body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "22:00:00",
+                latitude: 19.54823,
+                longitude: ""
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
+
+    it("Should validate longitude body value outside boundaries", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.ADMINISTRATOR });
+        const response = await request(app)
+            .put(`/api/stores/1`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                name: "New store name",
+                address: "New store address",
+                openingTime: "08:00:00",
+                closingTime: "22:00:00",
+                latitude: 19.54823,
+                longitude: -190
+            });
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+    });
 });
