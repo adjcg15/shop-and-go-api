cc78dd91a3163804f87ad79632d6fea7d84397a6
Angel Garcia <adjcg15@gmail.com>
2025-01-07 00:05:12 -0600

    featvalidation schemas): creates schema to validae body while creating a new incident

--- src/validation_schemas/incidents.ts
+++ src/validation_schemas/incidents.ts
@@ -21,6 +21,27 @@ const getIncidentsListValidationSchema: Schema = {
     }
 };
 
+const createIncidentValidationSchema: Schema = {
+    idOrder: {
+        in: ["body"],
+        isInt: {
+            options: { min: 1 },
+            errorMessage: "Parameter idOrder must be a positive integer",
+        },
+        toInt: true,
+    },
+    reason: {
+        in: ["body"],
+        trim: true,
+        isLength: {
+            options: { max: 255, min: 1 },
+            errorMessage:
+                "Body value reason can't be ampty and must have a maximum length of 255 characters",
+        },
+    }
+};
+
 export {
-    getIncidentsListValidationSchema
+    getIncidentsListValidationSchema,
+    createIncidentValidationSchema
 };
