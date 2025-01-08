d6b45951b6db0892bfb61035fdeb0b9f2fbc2694
Angel Garcia <adjcg15@gmail.com>
2025-01-08 01:14:07 -0600

    test(createIncident): implements body format validation tests

--- src/__tests__/api/incidents/createIncident.test.ts
+++ src/__tests__/api/incidents/createIncident.test.ts
@@ -47,4 +47,50 @@ describe("POST /api/incidents", () => {
 
         expect(response.status).toBe(HttpStatusCodes.FORBIDDEN);
     });
+
+    it("Should validate idOrder not empty body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.DELIVERY_MAN });
+        const response = await request(app)
+            .post(`/api/incidents`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                reason: "New incident reason"
+            });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+        expect(response.body.details.length).toBeGreaterThan(0);
+    });
+
+    it("Should validate idOrder body value format", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.DELIVERY_MAN });
+        const response = await request(app)
+            .post(`/api/incidents`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                idOrder: "juan",
+                reason: "New incident reason"
+            });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+        expect(response.body.details.length).toBeGreaterThan(0);
+    });
+
+    it("Should validate reason not empty body value", async () => {
+        const token = signToken({ id: 1, userRole: UserRoles.DELIVERY_MAN });
+        const response = await request(app)
+            .post(`/api/incidents`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({
+                idOrder: 1,
+                reason: ""
+            });
+
+        expect(response.status).toBe(HttpStatusCodes.BAD_REQUEST);
+        expect(Array.isArray(response.body.details)).toBe(true);
+        expect(response.body.details.length).toBeGreaterThan(0);
+    });
+
+    
 });
