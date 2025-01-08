3ec1b696a6a7bdc7ffc852e4a7699c63f3d1fee8
Ángel de Jesús de la Cruz García <82688573+adjcg15@users.noreply.github.com>
2025-01-07 21:11:57 -0600

    FEAT: implementation of get delivery men available for work endpoint
    * feat(get delivery men list): implements service to recover all delivery men available for work on a specific store
    
    * feat(get delivery men list): implements endpoint controller and exposes it on app router

--- src/services/employees_service.ts
+++ src/services/employees_service.ts
@@ -9,6 +9,8 @@ import { CreateEmployeeErrorCodes, UpdateEmployeeErrorCodes } from "../types/enu
 import { ErrorMessages } from "../types/enums/error_messages";
 import UserRoles from "../types/enums/user_roles";
 import { IEmployeeWithPosition } from "../types/interfaces/response_bodies";
+import { InferAttributes } from "sequelize";
+import { HttpStatusCodes } from "../types/enums/http";
 
 async function getEmployeeById(id: number) {
     let employee: IEmployeeWithPosition;
@@ -175,8 +177,41 @@ async function updateEmployee(
     return updatedEmployee;
 }
 
+async function getDeliveryMenAvailableForWorkOnStore(idStore: number) {
+    const deliveryMen: Omit<InferAttributes<Employee>, "passwordHash">[] = [];
+
+    try {
+        const dbDeliveryManPositon = await db.EmployeePosition.findOne({
+            where: { title: UserRoles.DELIVERY_MAN },
+        });
+        if (!dbDeliveryManPositon) {
+            throw new BusinessLogicException(
+                "The employee position DELIVERY_MAN is not registered on database and is needed",
+                undefined,
+                HttpStatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        const dbDeliveryMen = await db.Employee.findAll({
+            where: { isAvailableForWork: true, idStore, idPosition: dbDeliveryManPositon.id }
+        });
+
+
+        dbDeliveryMen.forEach(employee => {
+            const { passwordHash, ...employeeWithoutPassword} = employee.toJSON();
+
+            deliveryMen.push(employeeWithoutPassword);
+        });
+    } catch (error: any) {
+        throw new SQLException(error);
+    }
+
+    return deliveryMen;
+}
+
 export {
     getEmployeeById,
     createEmployee,
-    updateEmployee
+    updateEmployee,
+    getDeliveryMenAvailableForWorkOnStore
 };
